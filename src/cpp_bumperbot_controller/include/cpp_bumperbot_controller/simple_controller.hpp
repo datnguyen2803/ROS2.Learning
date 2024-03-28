7d3e989f9f6ccb50d95d33c6883dde50ca650f48
hvn_ntdat <dat10hoa1@gmail.com>
2024-03-28 17:55:32 +0700

    upto 103

--- src/cpp_bumperbot_controller/include/cpp_bumperbot_controller/simple_controller.hpp
+++ src/cpp_bumperbot_controller/include/cpp_bumperbot_controller/simple_controller.hpp
@@ -4,6 +4,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <std_msgs/msg/float64_multi_array.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <Eigen/Core>
 
 class SimpleController : public rclcpp::Node
@@ -13,13 +15,27 @@ public:
 
 private:
 	void velCallback(const geometry_msgs::msg::TwistStamped &msg);
+	void jointCallback(const sensor_msgs::msg::JointState &msg);
 
 	rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
 	rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub_;
+	rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_sub_;
+	rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
 
 	double wheel_radius_;
 	double wheel_separation_;
 	Eigen::Matrix2d speed_conversion_;
+
+	double left_wheel_prev_pos_;
+	double right_wheel_prev_pos_;
+	rclcpp::Time prev_time_;
+
+	double x_;
+	double y_;
+	double theta_;
+
+	nav_msgs::msg::Odometry odom_msg_;
+
 
 
 };
